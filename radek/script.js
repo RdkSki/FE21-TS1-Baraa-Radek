60b45b704febfca2df0f829281c9db1c586b0b84
RdkSki <rotax@gmx.net>
2021-10-06 13:29:22 +0200

    start

---
+++ radek/script.js
@@ -0,0 +1,22 @@
+// const array = [1,2,3,4,5,6,7,8,9,10];
+// for (let i=1; i<array.length, i++){
+// for (let val of Object.entries(array)) {
+//   let y= i * [val];
+//    console.log(y);
+// }
+// }
+// // 20
+// 30
+// for (let k = 0; k < myArray.length; k++) {
+//    function multiply(){
+//   let i=1;i<11;i++
+//     let y = myArray[k] * i;
+//     document.getElementById("xxx").innerHTML=""
+// }
+// }
+// function multiply(){
+//   for(i=1;i<11;i++){
+//     let y = val.myArray[0] * i;
+//     console.log(y);
+// }
+//   }
